262965a5c0acf68a5ab527e57c931346ee711257
Milen Valchev <milen.ovalchev@gmail.com>
2026-02-09 11:54:29 +0200

    init skeleton

---
+++ pocketr-api/src/main/kotlin/com/decrux/pocketr_api/PocketrApiApplication.kt
@@ -0,0 +1,11 @@
+package com.decrux.pocketr_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocketrApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PocketrApiApplication>(*args)
+}
---
+++ pocketr-api/src/test/kotlin/com/decrux/pocketr_api/PocketrApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.decrux.pocketr_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocketrApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
